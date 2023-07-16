f865bdf6ee8067e756a4be9a7ade178de22954c7
Akorede <semiola90@gmail.com>
2023-07-16 10:06:11 +0000

    prints string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
 * main - prints string
-* 
+*
 * Return: Always 0 (Success)
 */
-int main(void) {
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
