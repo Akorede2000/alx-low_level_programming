738e08f38439b4989b3d0a84cbe174653ec699c2
Akorede <semiola90@gmail.com>
2023-07-20 22:28:50 +0000

    Print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,16 +6,15 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	for (i = 0; i < n; i++)
+	for(i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
+		for(j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
