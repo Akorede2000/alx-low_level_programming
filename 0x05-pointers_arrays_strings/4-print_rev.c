7d5134fd453743feb8d6f5a70cd3f45b64f59fee
Akorede <semiola90@gmail.com>
2023-07-24 22:01:26 +0000

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ * @s: pointer
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
