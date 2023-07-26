3be0742fb7add569bd6e65fe873ae369787cb8c3
Akorede <semiola90@gmail.com>
2023-07-26 15:10:35 +0000

    Concat string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concat strings
+ * @dest: pointer
+ * @src: pointer
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len;
+
+	i = 0, len = 0;
+	while (dest[i])
+	{
+		i++;
+		len++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
