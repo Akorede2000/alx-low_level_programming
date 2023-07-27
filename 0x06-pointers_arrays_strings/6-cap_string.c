5be0e2b7dda07679c220c9aba1ee30acbefea32a
Akorede <semiola90@gmail.com>
2023-07-27 08:47:58 +0000

    Capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,38 @@
 #include "main.h"
-
 /**
- * cap_string - cap
- * @s: s
- * Return: char
+ * _indexOf - returns boolean if special  character
+ * @a: character to return
+ * Return: true or false
  */
+int _indexOf(char a)
+{
+	int i;
+	char capArr[13] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(',
+')', '{', '}'};
 
+	for (i = 0; i < 13; i++)
+	{
+		if (capArr[i] == a)
+			return (1);
+	}
+	return (0);
+}
+/**
+ * cap_string - capitalizes the string
+ * @s: string
+ * Return: the string capitalized
+ */
 char *cap_string(char *s)
 {
-	int i, diff, j, len;
-	char a[] = " \t\n,;.!?\"(){}";
+	int i;
 
-	diff = 'a' - 'A';
-	len = 0;
-	while (a[len] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		len++;
-	}
+		if (_indexOf(s[i]))
+			continue;
+		if (s[i] >= 'a' && s[i] <= 'z' && (_indexOf(s[i - 1]) || i == 0))
+			s[i] = s[i] - 32;
 
-	for (j = 0; j < len; j++)
-	{
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			if (s[i - 1] == a[j])
-			{
-				if (s[i] <= 'z' && s[i] >= 'a')
-				{
-					s[i] = s[i] - diff;
-				}
-				else
-				{
-					s[i] = s[i];
-				}
-			}
-		}
 	}
 	return (s);
 }
